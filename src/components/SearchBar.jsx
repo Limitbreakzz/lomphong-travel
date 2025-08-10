c50b2a83b095c692d57b0b092c3cce105f476a4e
Limitbreakzz <lomphongsrathongmee@gmail.com>
2025-08-10 19:01:03 +0700

    first commit

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -6,7 +6,7 @@ function SearchBar({ placeholder, onSearchChange }) {
       type="text"
       placeholder={placeholder}
       onChange={onSearchChange}
-      className="w-full p-3 text-base border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+      className="w-full p-3 text-base border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-700 focus:border-transparent"
     />
   );
 }
